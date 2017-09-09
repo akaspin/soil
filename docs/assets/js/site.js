af8e39c7219a3897037e140c165c5ac31322fc9b
Alexander Dorofeev <aka.spin@gmail.com>
2017-09-09 03:42:56 +0200

    HCL syntax in docs

--- docs/assets/js/site.js
+++ docs/assets/js/site.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
-    // $('.content').on('mouseenter', 'h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]', function(e) {
-    //     $(e.target).append($('<a />').addClass('header-anchor').attr('href', '#' + e.target.id).html('<i class="fa fa-link " aria-hidden="true"></i>'));
-    // });
-    //
-    // $('.content').on('mouseleave', 'h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]', function(e) {
-    //     $(e.target).parent().find('.header-anchor').remove();
-    // });
 
         return $(".content h2").each(function(i, el) {
             var $el, icon, id;
@@ -19,11 +12,6 @@ $(document).ready(function(){
 });
 
 $(document).ready(function() {
-    console.info(hljs.getLanguage('javascript'));
-    hljs.registerLanguage('hcl', function (hljs) {
-        lang = jQuery.extend({}, hljs.getLanguage('javascript'));
-        return lang;
-    });
     $('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
     });

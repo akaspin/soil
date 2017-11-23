26656c5a98a11e2a1a1d538530e72250484d478d
Alexander Dorofeev <aka.spin@gmail.com>
2017-11-23 03:04:30 +0100

    add logging to evaluator state

--- agent/provision/evaluator_state.go
+++ agent/provision/evaluator_state.go
@@ -1,25 +1,23 @@
 package provision
 
 import (
+	"github.com/akaspin/logx"
 	"github.com/akaspin/soil/agent/allocation"
 	"sync"
 )
 
 type EvaluatorState struct {
-	mu sync.Mutex
+	log *logx.Log
+	mu  sync.Mutex
 
-	// Finished evaluations
-	finished map[string]*allocation.Pod
-
-	// Evaluations in progress
-	inProgress map[string]*allocation.Pod
-
-	// Pending allocations
-	pending map[string]*allocation.Pod
+	finished   map[string]*allocation.Pod // Finished evaluations
+	inProgress map[string]*allocation.Pod // Evaluations in progress
+	pending    map[string]*allocation.Pod // Pending allocations
 }
 
-func NewEvaluatorState(recovered allocation.Recovery) (s *EvaluatorState) {
+func NewEvaluatorState(log *logx.Log, recovered allocation.Recovery) (s *EvaluatorState) {
 	s = &EvaluatorState{
+		log:        log.WithTags("evaluator", "state"),
 		finished:   map[string]*allocation.Pod{},
 		inProgress: map[string]*allocation.Pod{},
 		pending:    map[string]*allocation.Pod{},
@@ -32,24 +30,30 @@ func NewEvaluatorState(recovered allocation.Recovery) (s *EvaluatorState) {
 
 // Submit allocation to state. Returns allocations ready to execute.
 func (s *EvaluatorState) Submit(name string, pod *allocation.Pod) (next []*Evaluation) {
+	s.log.Tracef(`submit: %s`, name)
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.pending[name] = pod
+	s.log.Tracef(`submit: registered pending %s`, name)
 	next = s.next()
 	return
 }
 
 // Commit in progress evaluation
 func (s *EvaluatorState) Commit(name string) (next []*Evaluation) {
+	s.log.Tracef(`commit: %s`, name)
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	if in := s.inProgress[name]; in != nil {
 		s.finished[name] = in
+		s.log.Tracef(`%s promoted to finished`, name)
 	} else {
 		delete(s.finished, name)
+		s.log.Tracef(`%s removed from finished`, name)
 	}
 	delete(s.inProgress, name)
+	s.log.Tracef(`%s removed from in progress`, name)
 	next = s.next()
 	return
 }
@@ -61,24 +65,32 @@ LOOP:
 			// blocked by inProgress
 			if allocation.IsEqual(inProgress, pending) {
 				delete(s.pending, pendingName)
+				s.log.Tracef(`pending %s removed: equal to in progress`, pendingName)
 			}
+			s.log.Tracef(`skip promote pending %s: in progress`, pendingName)
 			continue LOOP
 		}
 		// check for blockers
 		for _, finished := range s.finished {
-			if finished.Name != pendingName && allocation.IsBlocked(finished, pending) {
-				continue LOOP
+			if finished.Name != pendingName {
+				if err := allocation.IsBlocked(finished, pending); err != nil {
+					s.log.Warning(err)
+					continue LOOP
+				}
 			}
 		}
 		// not blocked
 		finished := s.finished[pendingName]
 		if allocation.IsEqual(finished, pending) {
 			delete(s.pending, pendingName)
+			s.log.Tracef(`pending %s removed: equal to finished`, pendingName)
 			continue LOOP
 		}
 		s.inProgress[pendingName] = pending
 		delete(s.pending, pendingName)
 		next = append(next, NewEvaluation(s.finished[pendingName], pending))
+		s.log.Tracef(`pending %s promoted to in progress`, pendingName)
 	}
+	s.log.Debugf(`next: %s`, next)
 	return
 }

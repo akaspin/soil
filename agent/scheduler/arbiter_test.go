0240b5158fb42bb8fb876f08ff27903b1ee0e76d
Alexander Dorofeev <aka.spin@gmail.com>
2018-01-07 07:40:41 +0100

    fix races in tests

--- agent/scheduler/arbiter_test.go
+++ agent/scheduler/arbiter_test.go
@@ -8,12 +8,14 @@ import (
 	"github.com/akaspin/logx"
 	"github.com/akaspin/soil/agent/bus"
 	"github.com/akaspin/soil/agent/scheduler"
+	"github.com/akaspin/soil/fixture"
 	"github.com/akaspin/soil/manifest"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"regexp"
+	"strconv"
 	"sync"
 	"testing"
-	"time"
 )
 
 func TestRegex(t *testing.T) {
@@ -50,6 +52,23 @@ func (e *dummyArbiterEntity) notify(err error, message bus.Message) {
 	e.messages = append(e.messages, message)
 }
 
+func (e *dummyArbiterEntity) checkErrorsFn(errs ...error) func() (err error) {
+	return func() (err error) {
+		e.mu.Lock()
+		defer e.mu.Unlock()
+		for i, err1 := range e.errors {
+			if err1 == nil && errs[i] == nil {
+				continue
+			}
+			if fmt.Sprint(err1) != fmt.Sprint(errs[i]) {
+				err = fmt.Errorf("not equal [%d] (expected)%v != (actual)%v", i, errs[i], err)
+				return
+			}
+		}
+		return
+	}
+}
+
 func (e *dummyArbiterEntity) assertErrors(t *testing.T, errors []error) {
 	t.Helper()
 	for i, err := range e.errors {
@@ -65,160 +84,164 @@ func (e *dummyArbiterEntity) assertErrors(t *testing.T, errors []error) {
 }
 
 func TestArbiter_ConsumeMessage(t *testing.T) {
-	arbiter := scheduler.NewArbiter(context.Background(), logx.GetLog("test"), "test",
-		scheduler.ArbiterConfig{
-			Required: manifest.Constraint{"${drain}": "!= true"},
-			ConstraintOnly: []*regexp.Regexp{
-				regexp.MustCompile(`^status\.pod\..+`),
-			},
-		},
-	)
-	entity1 := &dummyArbiterEntity{}
-	entity2 := &dummyArbiterEntity{}
-	entity3 := &dummyArbiterEntity{}
+	for i := 0; i < 5; i++ {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
+			arbiter := scheduler.NewArbiter(context.Background(), logx.GetLog("test"), "test",
+				scheduler.ArbiterConfig{
+					Required: manifest.Constraint{"${drain}": "!= true"},
+					ConstraintOnly: []*regexp.Regexp{
+						regexp.MustCompile(`^status\.pod\..+`),
+					},
+				},
+			)
+			entity1 := &dummyArbiterEntity{}
+			entity2 := &dummyArbiterEntity{}
+			entity3 := &dummyArbiterEntity{}
 
-	assert.NoError(t, arbiter.Open())
+			assert.NoError(t, arbiter.Open())
 
-	t.Run("bind 1 and 2", func(t *testing.T) {
-		arbiter.Bind("1", manifest.Constraint{"${1}": "true"}, entity1.notify)
-		arbiter.Bind("2", manifest.Constraint{"${2}": "true"}, entity2.notify)
-		// assert no actions
-		assert.Len(t, entity1.errors, 0)
-		assert.Len(t, entity2.errors, 0)
-	})
-	t.Run("enable 1 and 2", func(t *testing.T) {
-		arbiter.ConsumeMessage(bus.NewMessage("", map[string]string{
-			"1": "true",
-			"2": "true",
-		}))
-		time.Sleep(time.Millisecond * 100)
-		assert.Equal(t, []error{nil}, entity1.errors)
-		assert.Equal(t, []error{nil}, entity2.errors)
-	})
-	t.Run("disable 2", func(t *testing.T) {
-		arbiter.ConsumeMessage(bus.NewMessage("", map[string]string{
-			"1": "true",
-		}))
-		time.Sleep(time.Millisecond * 100)
-		entity1.assertErrors(t, []error{
-			nil,
-			nil,
-		})
-		entity2.assertErrors(t, []error{
-			nil,
-			fmt.Errorf(`constraint failed: "${2}":"true" ("${2}":"true")`),
-		})
-	})
-	t.Run("drain on", func(t *testing.T) {
-		arbiter.ConsumeMessage(bus.NewMessage("", map[string]string{
-			"drain": "true",
-		}))
-		time.Sleep(time.Millisecond * 100)
-		entity1.assertErrors(t, []error{
-			nil,
-			nil,
-			fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
-		})
-		entity2.assertErrors(t, []error{
-			nil,
-			fmt.Errorf(`constraint failed: "${2}":"true" ("${2}":"true")`),
-			fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
-		})
-	})
-	t.Run("drain off", func(t *testing.T) {
-		arbiter.ConsumeMessage(bus.NewMessage("", map[string]string{
-			"1": "true",
-			"2": "true",
-		}))
-		time.Sleep(time.Millisecond * 100)
-		entity1.assertErrors(t, []error{
-			nil,
-			nil,
-			fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
-			nil,
-		})
-		entity2.assertErrors(t, []error{
-			nil,
-			fmt.Errorf(`constraint failed: "${2}":"true" ("${2}":"true")`),
-			fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
-			nil,
-		})
-	})
-	t.Run("bind 3", func(t *testing.T) {
-		arbiter.Bind("3", manifest.Constraint{"${2}": "true"}, entity3.notify)
-		time.Sleep(time.Millisecond * 100)
-		entity1.assertErrors(t, []error{
-			nil,
-			nil,
-			fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
-			nil,
-		})
-		entity2.assertErrors(t, []error{
-			nil,
-			fmt.Errorf(`constraint failed: "${2}":"true" ("${2}":"true")`),
-			fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
-			nil,
-		})
-		entity3.assertErrors(t, []error{
-			nil,
-		})
-	})
-	t.Run("unbind 1", func(t *testing.T) {
-		arbiter.Unbind("1", func() {
-			entity1.notify(fmt.Errorf("unbind"), bus.NewMessage("", nil))
-		})
-		time.Sleep(time.Millisecond * 100)
-		entity1.assertErrors(t, []error{
-			nil,
-			nil,
-			fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
-			nil,
-			fmt.Errorf("unbind"),
-		})
-	})
-	t.Run("bind 1 with status constraint", func(t *testing.T) {
-		arbiter.Bind("1", manifest.Constraint{
-			"${1}":            "true",
-			"${status.pod.1}": "ok",
-		}, entity1.notify)
-		time.Sleep(time.Millisecond * 100)
-		entity1.assertErrors(t, []error{
-			nil,
-			nil,
-			fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
-			nil,
-			fmt.Errorf("unbind"),
-			fmt.Errorf("constraint failed: \"${status.pod.1}\":\"ok\" (\"${status.pod.1}\":\"ok\")"),
-		})
-	})
-	t.Run("update with constraintOnly", func(t *testing.T) {
-		arbiter.ConsumeMessage(bus.NewMessage("private", map[string]string{
-			"1":            "true",
-			"2":            "true",
-			"3":            "true",
-			"status.pod.1": "ok",
-		}))
-		time.Sleep(time.Millisecond * 100)
-		entity1.assertErrors(t, []error{
-			nil,
-			nil,
-			fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
-			nil,
-			fmt.Errorf("unbind"),
-			fmt.Errorf("constraint failed: \"${status.pod.1}\":\"ok\" (\"${status.pod.1}\":\"ok\")"),
-			nil,
-		})
-		lastMsg := entity1.messages[len(entity1.messages)-1]
-		var chunk map[string]string
-		assert.NoError(t, lastMsg.Payload().Unmarshal(&chunk))
+			t.Run("bind 1 and 2", func(t *testing.T) {
+				arbiter.Bind("1", manifest.Constraint{"${1}": "true"}, entity1.notify)
+				arbiter.Bind("2", manifest.Constraint{"${2}": "true"}, entity2.notify)
+				// assert no actions
+				assert.Len(t, entity1.errors, 0)
+				assert.Len(t, entity2.errors, 0)
+			})
+			t.Run("enable 1 and 2", func(t *testing.T) {
+				arbiter.ConsumeMessage(bus.NewMessage("", map[string]string{
+					"1": "true",
+					"2": "true",
+				}))
+				fixture.WaitNoError10(t, entity1.checkErrorsFn(nil))
+				fixture.WaitNoError10(t, entity2.checkErrorsFn(nil))
+			})
+			t.Run("disable 2", func(t *testing.T) {
+				arbiter.ConsumeMessage(bus.NewMessage("", map[string]string{
+					"1": "true",
+				}))
+				fixture.WaitNoError10(t, entity1.checkErrorsFn(nil, nil))
+				fixture.WaitNoError10(t, entity2.checkErrorsFn(
+					nil,
+					fmt.Errorf(`constraint failed: "${2}":"true" ("${2}":"true")`),
+				))
+			})
+			t.Run("drain on", func(t *testing.T) {
+				arbiter.ConsumeMessage(bus.NewMessage("", map[string]string{
+					"drain": "true",
+				}))
+				fixture.WaitNoError10(t, entity1.checkErrorsFn(
+					nil,
+					nil,
+					fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
+				))
+				fixture.WaitNoError10(t, entity2.checkErrorsFn(
+					nil,
+					fmt.Errorf(`constraint failed: "${2}":"true" ("${2}":"true")`),
+					fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
+				))
+			})
+			t.Run("drain off", func(t *testing.T) {
+				arbiter.ConsumeMessage(bus.NewMessage("", map[string]string{
+					"1": "true",
+					"2": "true",
+				}))
+				fixture.WaitNoError10(t, entity1.checkErrorsFn(
+					nil,
+					nil,
+					fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
+					nil,
+				))
+				fixture.WaitNoError10(t, entity2.checkErrorsFn(
+					nil,
+					fmt.Errorf(`constraint failed: "${2}":"true" ("${2}":"true")`),
+					fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
+					nil,
+				))
+			})
+			t.Run("bind 3", func(t *testing.T) {
+				arbiter.Bind("3", manifest.Constraint{"${2}": "true"}, entity3.notify)
+				fixture.WaitNoError10(t, entity1.checkErrorsFn(
+					nil,
+					nil,
+					fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
+					nil,
+				))
+				fixture.WaitNoError10(t, entity2.checkErrorsFn(
+					nil,
+					fmt.Errorf(`constraint failed: "${2}":"true" ("${2}":"true")`),
+					fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
+					nil,
+				))
+				fixture.WaitNoError10(t, entity3.checkErrorsFn(
+					nil,
+				))
+			})
+			t.Run("unbind 1", func(t *testing.T) {
+				arbiter.Unbind("1", func() {
+					entity1.notify(fmt.Errorf("unbind"), bus.NewMessage("", nil))
+				})
+				fixture.WaitNoError10(t, entity1.checkErrorsFn(
+					nil,
+					nil,
+					fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
+					nil,
+					fmt.Errorf("unbind"),
+				))
+			})
+			t.Run("bind 1 with status constraint", func(t *testing.T) {
+				arbiter.Bind("1", manifest.Constraint{
+					"${1}":            "true",
+					"${status.pod.1}": "ok",
+				}, entity1.notify)
+				fixture.WaitNoError10(t, entity1.checkErrorsFn(
+					nil,
+					nil,
+					fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
+					nil,
+					fmt.Errorf("unbind"),
+					fmt.Errorf("constraint failed: \"${status.pod.1}\":\"ok\" (\"${status.pod.1}\":\"ok\")"),
+				))
+			})
+			t.Run("update with constraintOnly", func(t *testing.T) {
+				arbiter.ConsumeMessage(bus.NewMessage("private", map[string]string{
+					"1":            "true",
+					"2":            "true",
+					"3":            "true",
+					"status.pod.1": "ok",
+				}))
+				fixture.WaitNoError10(t, entity1.checkErrorsFn(
+					nil,
+					nil,
+					fmt.Errorf(`constraint failed: "true":"!= true" ("${drain}":"!= true")`),
+					nil,
+					fmt.Errorf("unbind"),
+					fmt.Errorf("constraint failed: \"${status.pod.1}\":\"ok\" (\"${status.pod.1}\":\"ok\")"),
+					nil,
+				))
+				fixture.WaitNoError10(t, func() (err error) {
+					expect := map[string]string{
+						"1": "true",
+						"2": "true",
+						"3": "true",
+					}
+					entity1.mu.Lock()
+					defer entity1.mu.Unlock()
+					lastMsg := entity1.messages[len(entity1.messages)-1]
+					var chunk map[string]string
+					if err = lastMsg.Payload().Unmarshal(&chunk); err != nil {
+						return
+					}
+					if !reflect.DeepEqual(expect, chunk) {
+						err = fmt.Errorf(`%v != %v`, expect, chunk)
+					}
+					return
+				})
+			})
 
-		assert.Equal(t, map[string]string{
-			"1": "true",
-			"2": "true",
-			"3": "true",
-		}, chunk)
-	})
+			arbiter.Close()
+			arbiter.Wait()
+		})
+	}
 
-	arbiter.Close()
-	arbiter.Wait()
 }
--- agent/scheduler/sink_test.go
+++ agent/scheduler/sink_test.go
@@ -8,14 +8,16 @@ import (
 	"github.com/akaspin/logx"
 	"github.com/akaspin/soil/agent/bus"
 	"github.com/akaspin/soil/agent/scheduler"
+	"github.com/akaspin/soil/fixture"
 	"github.com/akaspin/soil/lib"
 	"github.com/akaspin/soil/manifest"
 	"github.com/akaspin/supervisor"
 	"github.com/mitchellh/hashstructure"
 	"github.com/stretchr/testify/assert"
+	"reflect"
+	"strconv"
 	"sync"
 	"testing"
-	"time"
 )
 
 type dummyEvRecord struct {
@@ -64,123 +66,124 @@ func (e *dummyEv) Deallocate(name string) {
 	e.records[name] = append(e.records[name], dummyEvRecord{})
 }
 
+func (e *dummyEv) assertFn(recs map[string][]dummyEvRecord) func() (err error) {
+	return func() (err error) {
+		e.mu.Lock()
+		defer e.mu.Unlock()
+		if !reflect.DeepEqual(recs, e.records) {
+			err = fmt.Errorf("%v != %v", recs, e.records)
+		}
+		return
+	}
+}
+
 func TestSink_ConsumeRegistry(t *testing.T) {
-	ctx := context.Background()
-	log := logx.GetLog("test")
+	for i := 0; i < 5; i++ {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			log := logx.GetLog("test")
 
-	arbiter1 := scheduler.NewArbiter(ctx, log, "a1",
-		scheduler.ArbiterConfig{
-			Required: manifest.Constraint{
-				"${drain}": "!= true",
-			},
-		},
-	)
-	evaluator1 := &dummyEv{}
-	sink := scheduler.NewSink(ctx, log, nil, scheduler.NewBoundedEvaluator(arbiter1, evaluator1))
-	sv := supervisor.NewChain(ctx, arbiter1, sink)
-	assert.NoError(t, sv.Open())
+			arbiter1 := scheduler.NewArbiter(ctx, log, "a1",
+				scheduler.ArbiterConfig{
+					Required: manifest.Constraint{
+						"${drain}": "!= true",
+					},
+				},
+			)
+			evaluator1 := &dummyEv{}
+			sink := scheduler.NewSink(ctx, log, nil, scheduler.NewBoundedEvaluator(arbiter1, evaluator1))
+			sv := supervisor.NewChain(ctx, arbiter1, sink)
+			assert.NoError(t, sv.Open())
 
-	time.Sleep(time.Millisecond * 100)
+			t.Run("0 consume", func(t *testing.T) {
+				var buffers lib.StaticBuffers
+				var registry manifest.PodSlice
+				assert.NoError(t, buffers.ReadFiles("testdata/sink_test_ConsumeRegistry_0.hcl"))
+				assert.NoError(t, registry.Unmarshal(manifest.PrivateNamespace, buffers.GetReaders()...))
+				sink.ConsumeRegistry(registry)
+				fixture.WaitNoError10(t, evaluator1.assertFn(nil))
+			})
+			t.Run("1 enable first and second", func(t *testing.T) {
+				arbiter1.ConsumeMessage(bus.NewMessage("", map[string]string{
+					"meta.first":  "true",
+					"meta.second": "true",
+				}))
+				fixture.WaitNoError10(t, evaluator1.assertFn(map[string][]dummyEvRecord{
+					"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
+					"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
+					"third":  {{alloc: false, pod: 0x0, env: 0x0}},
+				}))
+			})
+			t.Run("2 modify third", func(t *testing.T) {
+				var buffers lib.StaticBuffers
+				var registry manifest.PodSlice
+				assert.NoError(t, buffers.ReadFiles("testdata/sink_test_ConsumeRegistry_2.hcl"))
+				assert.NoError(t, registry.Unmarshal(manifest.PrivateNamespace, buffers.GetReaders()...))
+				sink.ConsumeRegistry(registry)
+				fixture.WaitNoError10(t, evaluator1.assertFn(map[string][]dummyEvRecord{
+					"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
+					"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
+					"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}},
+				}))
+			})
+			t.Run("3 deactivate", func(t *testing.T) {
+				arbiter1.ConsumeMessage(bus.NewMessage("", nil))
+				fixture.WaitNoError10(t, evaluator1.assertFn(map[string][]dummyEvRecord{
+					"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
+					"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
+					"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}},
+				}))
+			})
+			t.Run("4 remove third", func(t *testing.T) {
+				var buffers lib.StaticBuffers
+				var registry manifest.PodSlice
+				assert.NoError(t, buffers.ReadFiles("testdata/sink_test_ConsumeRegistry_4.hcl"))
+				assert.NoError(t, registry.Unmarshal(manifest.PrivateNamespace, buffers.GetReaders()...))
+				sink.ConsumeRegistry(registry)
+				fixture.WaitNoError10(t, evaluator1.assertFn(map[string][]dummyEvRecord{
+					"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
+					"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
+					"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
+				}))
+			})
+			t.Run("5 activate", func(t *testing.T) {
+				arbiter1.ConsumeMessage(bus.NewMessage("", map[string]string{
+					"meta.first":  "true",
+					"meta.second": "true",
+				}))
+				fixture.WaitNoError10(t, evaluator1.assertFn(map[string][]dummyEvRecord{
+					"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
+					"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
+					"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
+				}))
 
-	t.Run("0 consume", func(t *testing.T) {
-		var buffers lib.StaticBuffers
-		var registry manifest.PodSlice
-		assert.NoError(t, buffers.ReadFiles("testdata/sink_test_ConsumeRegistry_0.hcl"))
-		assert.NoError(t, registry.Unmarshal(manifest.PrivateNamespace, buffers.GetReaders()...))
-		sink.ConsumeRegistry(registry)
-		time.Sleep(time.Millisecond * 100)
+			})
+			t.Run("6 drain", func(t *testing.T) {
+				arbiter1.ConsumeMessage(bus.NewMessage("", map[string]string{
+					"drain": "true",
+				}))
+				fixture.WaitNoError10(t, evaluator1.assertFn(map[string][]dummyEvRecord{
+					"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
+					"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
+					"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
+				}))
+			})
+			t.Run("7 remove drain", func(t *testing.T) {
+				arbiter1.ConsumeMessage(bus.NewMessage("", map[string]string{
+					"meta.first":  "true",
+					"meta.second": "true",
+				}))
+				fixture.WaitNoError10(t, evaluator1.assertFn(map[string][]dummyEvRecord{
+					"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
+					"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
+					"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
+				}))
+			})
 
-		assert.Nil(t, evaluator1.records, "no allocations")
-	})
-	t.Run("1 enable first and second", func(t *testing.T) {
-		arbiter1.ConsumeMessage(bus.NewMessage("", map[string]string{
-			"meta.first":  "true",
-			"meta.second": "true",
-		}))
-		time.Sleep(time.Millisecond * 100)
-		assert.Equal(t,
-			map[string][]dummyEvRecord{
-				"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
-				"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
-				"third":  {{alloc: false, pod: 0x0, env: 0x0}},
-			},
-			evaluator1.records)
-	})
-	t.Run("2 modify third", func(t *testing.T) {
-		var buffers lib.StaticBuffers
-		var registry manifest.PodSlice
-		assert.NoError(t, buffers.ReadFiles("testdata/sink_test_ConsumeRegistry_2.hcl"))
-		assert.NoError(t, registry.Unmarshal(manifest.PrivateNamespace, buffers.GetReaders()...))
-		sink.ConsumeRegistry(registry)
-		time.Sleep(time.Millisecond * 100)
+			sv.Close()
+			sv.Wait()
+		})
+	}
 
-		assert.Equal(t, map[string][]dummyEvRecord{
-			"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
-			"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
-			"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}},
-		},
-			evaluator1.records, "third should be updated")
-	})
-	t.Run("3 deactivate", func(t *testing.T) {
-		arbiter1.ConsumeMessage(bus.NewMessage("", nil))
-		time.Sleep(time.Millisecond * 100)
-		assert.Equal(t, map[string][]dummyEvRecord{
-			"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
-			"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
-			"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}},
-		}, evaluator1.records, "no updates: inactive")
-	})
-	t.Run("4 remove third", func(t *testing.T) {
-		var buffers lib.StaticBuffers
-		var registry manifest.PodSlice
-		assert.NoError(t, buffers.ReadFiles("testdata/sink_test_ConsumeRegistry_4.hcl"))
-		assert.NoError(t, registry.Unmarshal(manifest.PrivateNamespace, buffers.GetReaders()...))
-		sink.ConsumeRegistry(registry)
-		time.Sleep(time.Millisecond * 100)
-
-		assert.Equal(t, map[string][]dummyEvRecord{
-			"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
-			"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
-			"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
-		}, evaluator1.records, "no updates: inactive")
-	})
-	t.Run("5 activate", func(t *testing.T) {
-		arbiter1.ConsumeMessage(bus.NewMessage("", map[string]string{
-			"meta.first":  "true",
-			"meta.second": "true",
-		}))
-		time.Sleep(time.Millisecond * 100)
-		assert.Equal(t, map[string][]dummyEvRecord{
-			"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
-			"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
-			"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
-		}, evaluator1.records)
-
-	})
-	t.Run("6 drain", func(t *testing.T) {
-		arbiter1.ConsumeMessage(bus.NewMessage("", map[string]string{
-			"drain": "true",
-		}))
-		time.Sleep(time.Millisecond * 100)
-		assert.Equal(t, map[string][]dummyEvRecord{
-			"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
-			"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
-			"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
-		}, evaluator1.records, "drain")
-	})
-	t.Run("7 remove drain", func(t *testing.T) {
-		arbiter1.ConsumeMessage(bus.NewMessage("", map[string]string{
-			"meta.first":  "true",
-			"meta.second": "true",
-		}))
-		time.Sleep(time.Millisecond * 100)
-		assert.Equal(t, map[string][]dummyEvRecord{
-			"first":  {{alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0x1c2120eda6a20a7f, env: 0x88be0fba4063a209}},
-			"second": {{alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xadbac957c6a4641f, env: 0x88be0fba4063a209}},
-			"third":  {{alloc: false, pod: 0x0, env: 0x0}, {alloc: true, pod: 0xa0249adc0c01cf50, env: 0x88be0fba4063a209}, {alloc: false, pod: 0x0, env: 0x0}},
-		}, evaluator1.records, "remove drain")
-	})
-
-	sv.Close()
-	sv.Wait()
 }
